17cc4f58d9a30e874d745bca9ffa1a29992e6a9a
Matilda Briegel <matilda.briegel@tum.de>
2026-02-28 19:35:25 +0100

    Update resource class

--- resources/Resource.py
+++ resources/Resource.py
@@ -1,28 +1,33 @@
 class Resource:
-    def __init__(self, res_name):
-        self.name = res_name
-        self.role = None
-        self.occupied_until = None
+    def __init__(self, res_id):
+        self._id = res_id
+        self._role = None
+        self._is_occupied = False
+        self._task_queue = []
 
     def set_role(self, role):
-        self.role = role
+        self._role = role
 
     def get_role(self):
-        return self.role
+        return self._role
 
     def get_id(self):
-        return self.name
+        return self._id
 
-    def is_occupied(self, current_time):
-        if self.occupied_until is None:
-            return False
-        if current_time >= self.occupied_until:
-            self.release()
-            return False
-        return True
+    def add_task(self, task):
+        self._task_queue.append(task)
 
-    def occupy(self, until_time):
-        self.occupied_until = until_time
+    def pop_task(self):
+        return self._task_queue.pop(0)
+
+    def get_queue_length(self):
+        return len(self._task_queue)
+
+    def is_occupied(self):
+        return self._is_occupied
+
+    def occupy(self):
+        self._is_occupied = True
 
     def release(self):
-        self.occupied_until = None
+        self._is_occupied = False
