b0aa23664b688ec27bb884126b85c00218a0a5c4
Matilda Briegel <matilda.briegel@tum.de>
2026-03-08 13:40:16 +0100

    Fix bug in SHQ assignment and add ADVANCED assignment mock implementation

--- resources/Resource.py
+++ resources/Resource.py
@@ -18,7 +18,9 @@ class Resource:
         self._task_queue.append(task)
 
     def pop_task(self):
-        return self._task_queue.pop(0)
+        if len(self._task_queue) > 0:
+            return self._task_queue.pop(0)
+        return None
 
     def get_queue_length(self):
         return len(self._task_queue)
@@ -31,3 +33,18 @@ class Resource:
 
     def release(self):
         self._is_occupied = False
+
+    def get_remaining_working_time(self, current_now):
+        if not self._is_occupied or not self._task_queue:
+            return 0.0
+
+        remaining_seconds = 0.0
+        for i, task in enumerate(self._task_queue):
+            if i == 0:  # Current task
+                expected_end = task['start'] + task['duration']
+                remaining = (expected_end - current_now).total_seconds()
+                remaining_seconds += max(0, remaining)
+            else:   # Future already assigned tasks
+                remaining_seconds += task['duration'].total_seconds()
+
+        return remaining_seconds
